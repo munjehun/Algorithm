e16d0e3ca1ed8db99c819c48b3149496b51f68a6
munjehun <miso1489@gmail.com>
2022-08-24 12:31:22 +0900

    완주하지 못한 선수

---
+++ 알고리즘/5.완주하지 못한 선수.js
@@ -0,0 +1,53 @@
+/*
+-문제 설명
+수많은 마라톤 선수들이 마라톤에 참여하였습니다. 
+단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
+
+마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때,
+완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
+
+-제한사항
+마라톤 경기에 참여한 선수의 수는 1명 이상 100,000명 이하입니다.
+completion의 길이는 participant의 길이보다 1 작습니다.
+참가자의 이름은 1개 이상 20개 이하의 알파벳 소문자로 이루어져 있습니다.
+참가자 중에는 동명이인이 있을 수 있습니다.
+*/
+
+let a = ["leo", "kiki", "eden"];
+let b = ["eden", "kiki"];
+
+function solution(participant, completion) {
+  participant.sort();
+  completion.sort();
+
+  for (let i = 0; i < participant.length; i++) {
+    if (participant[i] !== completion[i]) {
+      let answer = participant[i];
+      return answer;
+    }
+  }
+}
+
+solution(a, b);
+// 이건 약간 잔머리?식으로 푼 방법인듯. 어쨌거나 못풀어서 결국 구글링해서 따라했다.
+
+/*다른 사람의 풀이
+
+function solution(participant, completion) {
+    const map = new Map();
+
+    for(let i = 0; i < participant.length; i++) {
+        let a = participant[i], 
+            b = completion[i];
+
+        map.set(a, (map.get(a) || 0) + 1);
+        map.set(b, (map.get(b) || 0) - 1);
+    }
+
+    for(let [k, v] of map) {
+        if(v > 0) return k;
+    }
+
+    return 'nothing';
+}
+*/
