3eb8eead97dabe02823bfec02edaaea07178b771
munjehun <miso1489@gmail.com>
2022-09-04 22:26:09 +0900

    220904 나누어 떨어지는 숫자 배열

--- 알고리즘/16.나누어 떨어지는 숫자 배열.js
+++ 알고리즘/16.나누어 떨어지는 숫자 배열.js
@@ -19,6 +19,7 @@ function solution(arr, divisor) {
   return arr.length === 0 ? [-1] : arr.sort((a, b) => a - b);
 }
 
-solution([3, 2, 6], 10);
+solution([3, 2, 6], 10); //[-1]
+solution([2, 36, 1, 3], 1); //[1, 2, 3, 36]
 
 // => [-1] 자체가 배열!
