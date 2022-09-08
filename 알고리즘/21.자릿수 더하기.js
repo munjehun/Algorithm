523fdf445018feb5c2de06518b067e91aded498b
munjehun <miso1489@gmail.com>
2022-09-08 22:46:52 +0900

    220908 자릿수 더하기

---
+++ 알고리즘/21.자릿수 더하기.js
@@ -0,0 +1,19 @@
+/*-문제 설명
+자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
+예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
+*/
+
+function solution(n) {
+  console.log(
+    n
+      .toString()
+      .split("")
+      .reduce((a, b) => +a + +b)
+  );
+  return n
+    .toString()
+    .split("")
+    .reduce((a, b) => +a + +b);
+}
+
+solution(123);
