a268e54c2f9536ae8efafe06276254115e449909
munjehun <miso1489@gmail.com>
2022-09-12 15:43:56 +0900

    220912 문자열 내림차순으로 배치하기

---
+++ 알고리즘/32.문자열 내림차순으로 배치하기.js
@@ -0,0 +1,10 @@
+/*-문제 설명
+문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴하는 함수를 완성해주세요.
+s는 영문 대소문자로만 구성되어 있으며, 대문자는 소문자보다 작은 것으로 간주합니다.
+*/
+
+function solution(s) {
+  return s.split("").sort().reverse().join("");
+}
+
+solution("Zbcdefg");
