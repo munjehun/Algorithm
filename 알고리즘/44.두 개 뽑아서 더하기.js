72cd726bfc101e08c9f5a1a89724a1e2d5797b73
munjehun <miso1489@gmail.com>
2022-09-21 17:38:04 +0900

    220921 두 개 뽑아서 더하기

--- 알고리즘/44.두 개 뽑아서 더하기.js
+++ 알고리즘/44.두 개 뽑아서 더하기.js
@@ -2,12 +2,6 @@
 정수 배열 numbers가 주어집니다. 
 numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아 return 하도록 함수를 완성해주세요.
 */
-/*
-1.for문 2개 중첩으로 돌려서 2개 인자의 합 구하기
-2. 빈 배열 result에 더하기
-3. result에 그 수가 없을때만 더하기
-4. return result sort((a,b)=> a-b);
-*/
 
 function solution(numbers) {
   let result = [];
@@ -25,8 +19,8 @@ function solution(numbers) {
 solution([2, 1, 3, 4, 1]);
 solution([5, 0, 2, 7]);
 
-// => ...new Set(arr) : arr의 원소를 중복없는 객체로 만들기
-// => [...new Set(arr)] : arr의 원소를 중복없는 배열로 만들기
+// 💯 ...new Set(arr) : arr의 원소를 중복없는 객체로 만들기
+// 💯 [...new Set(arr)] : arr의 원소를 중복없는 배열로 만들기
 
 /*
 🤖 다른 사람의 풀이
