79f1f050b5c59038d3a67d093343987a5a0c5e8a
munjehun <miso1489@gmail.com>
2022-12-06 20:14:56 +0900

    221204 삼총사 수정

--- 알고리즘/54.삼총사.js
+++ 알고리즘/54.삼총사.js
@@ -18,12 +18,22 @@
 */
 
 function solution(number) {
-  let sum = 0;
-  for (let i = 0; i < number.length; i++) {
-    for (let j = i + 1; j < number.length; j++) {
-      for (let k = j + 1; k < number.length; k++)
-        if (number[i] + number[j] + number[k] === 0) sum += 1;
+  let count = 0;
+  for (let i = 0; i < number.length - 2; i++) {
+    for (let j = i + 1; j < number.length - 1; j++) {
+      for (let k = j + 1; k < number.length; k++) {
+        if (number[i] + number[j] + number[k] == 0) count += 1;
+      }
     }
   }
-  return sum;
+  return count;
 }
+
+solution([-2, 3, 0, 2, -5]);
+solution([-3, -2, -1, 0, 1, 2, 3]);
+solution([-1, 1, -1, 1]);
+
+/*
+⚡️ 대부분의 다를 풀이를 봐도 for문 중첩으로 풀이하였다. 
+조합 방식으로 풀려 생각했지만 구현을 못했다.  
+*/
